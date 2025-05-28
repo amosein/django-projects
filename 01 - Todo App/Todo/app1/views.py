95bbd873fdade3d11c083a7a959cabf469125cf8
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-05-28 12:55:40 +0330

    added 3 html files and relate it to another and add a navbar too with bootstrap link in base.html and navbar in navbar.html and the main home page in home.html and also add it to seeting file and connect a view to it

--- 01 - Todo App/Todo/app1/views.py
+++ 01 - Todo App/Todo/app1/views.py
@@ -3,4 +3,5 @@ from django.http import  HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("This is Home Page !")
+    # return HttpResponse("This is Home Page !")
+    return render(request, "home.html", )
