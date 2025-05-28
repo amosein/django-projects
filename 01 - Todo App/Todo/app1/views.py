fcf770fb91d042547ed5a4dfc0a77b707cc87882
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-05-28 13:26:08 +0330

    showing all the records in databases in html files and made some good changes in home.html file to show it better.

--- 01 - Todo App/Todo/app1/views.py
+++ 01 - Todo App/Todo/app1/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+from .models import Todo
 
 # Create your views here.
 def home(request):
+    todos = Todo.objects.all()
     # return HttpResponse("This is Home Page !")
-    return render(request, "home.html", )
+    return render(request, "home.html", context={"todos": todos})
