b7273a45ff16777743e159ddd146854ab92f4728
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-05-29 11:47:03 +0330

    some few bugs i had, i solve them it was for naming and few spaces and wrong name for my files and now my home.html have title that likns to detial.html page and work just fine

--- 01 - Todo App/Todo/app1/views.py
+++ 01 - Todo App/Todo/app1/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
-
-from University_Project.Test.c6 import total_data
 from .models import Todo
 
 # Create your views here.
@@ -12,4 +10,4 @@ def home(request):
 
 def detail(request, id):
     todo_data = Todo.objects.get(id=id)
-    return render(request, "detail", context={"data": todo_data})
+    return render(request, "detail.html", context={"data": todo_data})
