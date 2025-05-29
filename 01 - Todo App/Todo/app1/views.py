28ce7f06f70a315e98dbd87afb07a556d27a5927
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-05-29 11:39:34 +0330

    update my home.html in order that my title now have a link connected to a function and the detail for them is now in that new tab named detail

--- 01 - Todo App/Todo/app1/views.py
+++ 01 - Todo App/Todo/app1/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+
+from University_Project.Test.c6 import total_data
 from .models import Todo
 
 # Create your views here.
@@ -7,3 +9,7 @@ def home(request):
     todos = Todo.objects.all()
     # return HttpResponse("This is Home Page !")
     return render(request, "home.html", context={"todos": todos})
+
+def detail(request, id):
+    todo_data = Todo.objects.get(id=id)
+    return render(request, "detail", context={"data": todo_data})
