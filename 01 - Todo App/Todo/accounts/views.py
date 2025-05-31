6fcd9612031d9c1ba55707954c3fd1b8da0ab2bb
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-05-31 10:31:11 +0330

    add a field in bootstrap and connect it to the user registe to have easy access.

--- 01 - Todo App/Todo/accounts/views.py
+++ 01 - Todo App/Todo/accounts/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import *
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def user_register(request):
     if request.method == "POST":
-        pass
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd["username"], cd["email"], cd["password"])
+            messages.success(request, "User Created Successfully", "success")
+            return redirect("home")
     else:
         form = RegisterUserForm()
     return render(request, "register.html", {"form": form})
